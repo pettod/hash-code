8304f8a58cdf39d78eb1f2ab5ec7ef68cc5a5c3c
Peter Todorov <peter.todorov@live.com>
2021-02-27 20:37:38 +0200

    Add new version of hashcode template

---
+++ hashcode_template.cpp
@@ -0,0 +1,116 @@
+#include <vector>
+#include <map>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <set>
+#include <sstream>
+#include <functional>
+#include <iterator>
+#include <random>
+#include <list>
+#include <queue>
+#include <cstdlib>
+#include <string>
+#include <memory.h>
+#include <cstdint>
+#include <unordered_map>
+#include <unordered_set>
+#include <cassert>
+#include <bitset>
+#include <fstream>
+#include <deque>
+#include <cmath>
+#include <numeric>
+#include <stack>
+#include <stdarg.h>
+#include <cstdio>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+#define fi(n) for (int i = 0; i < int (n); i++)
+#define fj(n) for (int j = 0; j < int (n); j++)
+#define fk(n) for (int k = 0; k < int (n); k++)
+#define f1(_n) for (auto&i1 : _n)
+#define f2(_n) for (auto&i2 : _n)
+#define f3(_n) for (auto&i3 : _n)
+#define all(n) n.begin(), n.end()
+#define break(_x)    {_x;break;}
+#define continue(_x) {_x;continue;}
+#define _sort(_x)    sort(_x.begin(), _x.end())
+#define r_sort(_x)   sort(_x.rbegin(), _x.rend())
+#define MAX_VAL_OF(_x) std::numeric_limits<decltype(_x)>::max()
+#define v_int vector<int>
+
+int sum(vector<int> v) { int total = 0; fi(v.size()) total += v[i]; return total; }
+float mean(vector<int> v) { return sum(v) / v.size(); }
+float standardDeviation(vector<int> v) { float mu = mean(v); float s = 0; fi(v.size()) s += pow(v[i] - mu, 2); return sqrt(s / v.size()); }
+void reverseStringIntMap(map<string, int>& input_map, map<int, string>& output_map) { for (map<string, int>::iterator it = input_map.begin(); it != input_map.end(); it++) output_map.insert({it->second, it->first}); }
+
+// Implement write function
+void writeFile(string file_name)
+{
+    ofstream file;
+    file.open(file_name);
+    file << solution.size() << "\n";
+    fi(solution.size())
+    {
+        file << solution[i].first << "\n";
+        file << solution[i].second.size() << "\n";
+        fj(solution[i].second.size())
+            file << solution[i].second[j].first << " " << solution[i].second[j].second << "\n";
+    }
+    file.close();
+}
+
+// Add here all data structures when reading the file
+vector<pair<int, vector<pair<string, int>>>> solution;
+
+// Read file to data structures
+void readFile(string file_name)
+{
+    fstream file;
+    file.open(file_name, ios::in);
+    int a, b, c;
+    file >> a >> b >> c;
+
+    // read here
+    // Comparator example: [](auto& i, auto& j) { return i > j; }
+
+    file.close();
+}
+
+// Create solution vector
+void solve()
+{
+}
+
+// Change file names
+int main()
+{
+    vector<string> in_files = {
+        "input_files/a.txt",
+        "input_files/b.txt",
+        "input_files/c.txt",
+        "input_files/d.txt",
+        "input_files/e.txt",
+        "input_files/f.txt",
+    };
+    vector<string> out_files = {
+        "a_solution.txt",
+        "b_solution.txt",
+        "c_solution.txt",
+        "d_solution.txt",
+        "e_solution.txt",
+        "f_solution.txt",
+    };
+    fi(in_files.size())
+    {
+        readFile(in_files[i]);
+        solve();
+        writeFile(out_files[i]);
+    }
+}
